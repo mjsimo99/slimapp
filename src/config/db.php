11b4fe50948eb0322d5c0bbf85a5095e57874454
mjsimo99 <mohashark21@gmail.com>
2023-01-06 16:16:19 +0100

    update

---
+++ src/config/db.php
@@ -0,0 +1,18 @@
+<?php
+class db{
+    //Propeties
+    private $dbhost = 'localhost';
+    private $dbuser = 'root';
+    private $dbpass ='';
+    private $dbname ='slimapp';
+
+    // Connect
+    
+    public function connect(){
+        $mysql_connect_str = "mysql:host=$this->dbhost;dbname=$this->dbname" ;
+        $dbConnection = new PDO($mysql_connect_str,$this->dbuser,$this->dbpass);
+        $dbConnection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    return $dbConnection;
+    }
+    
+}
